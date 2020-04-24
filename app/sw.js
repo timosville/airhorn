b5336f8e3fe1912330f96e819e679a0eff1d30d6
Timothy Koech <timosville@gmail.com>
2020-04-24 17:10:01 +0300

    Intercept the web page requests

--- app/sw.js
+++ app/sw.js
@@ -14,3 +14,11 @@ self.addEventListener("install", (event) => {
     caches.open(cacheName).then((cache) => cache.addAll(cacheResources))
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
